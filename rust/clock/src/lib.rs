cf0ecbef5f3a202fb45200010c0d521367218ba6
Ethan Knowlton <eknowlton@gmail.com>
2019-06-17 16:49:06 -0400

    working

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,14 +1,34 @@
 use std::fmt;
 
+const DAY: i32 = 24 * 60;
+const HOUR: i32 = 60;
+
 pub struct Clock {
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        const TWENTY_FOUR_HOURS: i32 = 24 * 60;
-        let minutes = (hours * 60) + minutes + TWENTY_FOUR_HOURS;
-        Clock { minutes: minutes }
+        let minutes = hours * HOUR + minutes;
+        Clock { minutes }
+    }
+
+    pub fn hour(&self) -> i32 {
+        let minutes = self.minutes % DAY;
+
+        match minutes {
+            minutes if minutes >= 0 => minutes / HOUR,
+            minutes => (DAY + minutes) / HOUR,
+        }
+    }
+
+    pub fn minute(&self) -> i32 {
+        let minutes = self.minutes % 60;
+
+        match minutes {
+            m if m < 0 => 60 + minutes,
+            m => m,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
@@ -16,44 +36,22 @@ impl Clock {
             minutes: self.minutes + minutes,
         }
     }
-
-    pub fn to_time(&self) -> (i32, i32) {
-        let mut hour = (self.minutes / 60) % 24;
-
-        let minutes = {
-            let hours = self.minutes as f32 / 60.0;
-            let minutes_left = hours - hours.floor();
-
-            let result = minutes_left * 60.0;
-
-            println!("{}", result);
-
-            result.round() as i32
-        };
-
-        if hour < 0 {
-            hour = 24 + hour;
-        }
-
-        (hour, minutes)
-    }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let (hours, minutes) = self.to_time();
         write!(
             f,
             "{}:{}",
-            format!("{:0>2}", hours),
-            format!("{:0>2}", minutes)
+            format!("{:0>2}", self.hour()),
+            format!("{:0>2}", self.minute())
         )
     }
 }
 
 impl PartialEq for Clock {
     fn eq(&self, other: &Self) -> bool {
-        self.to_time() == other.to_time()
+        (self.hour(), self.minute()) == (other.hour(), other.minute())
     }
 }
 
