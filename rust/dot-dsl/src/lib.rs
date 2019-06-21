e08e7123e07f85ff09ed4a56266cb271405b9e8e
Ethan Knowlton <eknowlton@gmail.com>
2019-06-21 09:03:45 -0400

    some progress, problem with vecs and arrays... might need slices?

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -2,11 +2,22 @@ pub mod graph {
     pub mod graph_items {
         pub mod node {
             #[derive(Clone, Debug, PartialEq)]
-            pub struct Node<'a>(&'a str);
+            pub struct Node<'a> {
+                message: &'a str,
+                attrs: Vec<super::attr::Attr>,
+            }
 
             impl<'a> Node<'a> {
-                pub fn new(content: &'a str) -> Self {
-                    Node(content)
+                pub fn new(message: &'a str) -> Self {
+                    Node {
+                        message,
+                        attrs: vec![],
+                    }
+                }
+
+                pub fn with_attrs(&'a mut self, attrs: &'a Vec<super::attr::Attr>) -> &'a mut Self {
+                    self.attrs.extend(attrs.to_vec());
+                    self
                 }
             }
 
@@ -14,12 +25,21 @@ pub mod graph {
 
         pub mod edge {
             #[derive(Clone)]
-            pub struct Edge;
+            pub struct Edge<'a> {
+                to: &'a str,
+                from: &'a str,
+            }
+
+            impl<'a> Edge<'a> {
+                pub fn new(to: &'a str, from: &'a str) -> Self {
+                    Edge { to, from }
+                }
+            }
 
         }
         pub mod attr {
 
-            #[derive(Clone)]
+            #[derive(Clone, PartialEq, Debug)]
             pub struct Attr;
 
         }
@@ -28,7 +48,7 @@ pub mod graph {
 
     pub struct Graph<'a> {
         pub nodes: Vec<graph_items::node::Node<'a>>,
-        pub edges: Vec<graph_items::edge::Edge>,
+        pub edges: Vec<graph_items::edge::Edge<'a>>,
         pub attrs: Vec<graph_items::attr::Attr>,
     }
 
